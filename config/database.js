5d43c0da030bfdcb2f6fc509ced04e5f7b9a295f
Nicole <nkatynsk@umich.edu>
2023-02-12 22:17:42 +0000

    Fixed database port

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     client: 'postgres',
     connection: {
       host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5433),
+      port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'strapidb'),
       user: env('DATABASE_USERNAME', 'strapi'),
       password: env('DATABASE_PASSWORD', '5uperDBA!'),
