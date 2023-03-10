e7c898c708970d30b89aedb420abe8bbff90cfb4
Nicole <nkatynsk@umich.edu>
2023-03-10 23:59:14 +0000

    Token

--- config/plugins.js
+++ config/plugins.js
@@ -1,23 +1,23 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: 'nodemailer',
-        providerOptions: {
-          host: env('NODEMAILER_SMTP_HOST'),
-          port: env('NODEMAILER_SMTP_PORT', 587),
-          auth: {
-            user: env('NODEMAILER_SMTP_USERNAME'),
-            pass: env('NODEMAILER_SMTP_PASSWORD'),
-          },
-          // ... any custom nodemailer options
+  // ...
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('NODEMAILER_SMTP_HOST'),
+        port: env('NODEMAILER_SMTP_PORT', 587),
+        auth: {
+          user: env('NODEMAILER_SMTP_USERNAME'),
+          pass: env('NODEMAILER_SMTP_PASSWORD'),
         },
-        settings: {
-          defaultFrom: env('NODEMAILER_FROM'),
-          defaultReplyTo: env('NODEMAILER_REPLYTO'),
-        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: env('NODEMAILER_FROM'),
+        defaultReplyTo: env('NODEMAILER_REPLYTO'),
       },
     },
-    // ...
-  });
-  
+  },
+
+  // ...
+});
